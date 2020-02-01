152aa5b22ef42a8b671e64b14a902c63cc87930c
Yeonggyun Kim <kygha7205@zerogyun.dev>
2020-02-01 17:31:06 +0900

    Add card vue component

---
+++ src/components/Card.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card"
+};
+</script>
+
+<style scoped>
+.card {
+  background-color: white;
+  color: white;
+  position: relative;
+  border-radius: 0.5em;
+  box-shadow:0 0 0.2rem 0.2rem rgba(0, 0, 0, 0.1);
+}
+</style>
