44f11a5d02c9edc61ecffc9d0827ecdd3958959d
Yeonggyun.Kim <kygha7205@naver.com>
2020-04-04 04:07:02 +0900

    Delete unused script

--- src/App.vue
+++ src/App.vue
@@ -10,13 +10,6 @@
 <script>
 export default {
   name: "app",
-  watch: {
-    '$route' (to, from) {
-      const toDepth = to.path.split('/').length
-      const fromDepth = from.path.split('/').length
-      this.fade = toDepth < fromDepth ? 'slide-right' : 'slide-left'
-    }
-  }
 };
 </script>
 
